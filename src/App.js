c65621a3694f7ad6eaab6da71a2e3f99eddf0f2a
am1macdonald <am1macdonald@gmail.com>
2022-07-06 18:27:03 -0400

    basic todo list app for playing around with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, {Component, useRef, useState} from "react";
+import Overview from "./components/Overview";
+import uniqid from "uniqid"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [],
+      task: {
+        value: "",
+        id: uniqid()
+       }
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      task: {
+        value: e.target.value,
+        id: this.state.task.id
+      }});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      tasks: [...this.state.tasks, this.state.task],
+      task: {
+        value: "",
+        id: uniqid()
+       }
+    })
+  }
+
+  render() {
+    return (
+          <div className="App">
+          <form>
+            <label htmlFor="input">Task</label>
+            <input type="text" id="input" name="input" value={this.state.task.value} onChange={this.handleChange}></input>
+            <button type="button" onClick={this.handleSubmit}>Submit</button>
+          </form>
+          <Overview tasks={this.state.tasks}/>
+        </div>)
+  }
 }
 
 export default App;
